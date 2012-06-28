e2ff827dd76027648cd6e20e871be044a967a956
Devimplode <jyhm.beem@googlemail.com>
2012-06-29 00:51:24 +0200

    default storage
    loading defined storage objects on page start
    new has function

--- class/storagemanager.php
+++ class/storagemanager.php
@@ -10,6 +10,9 @@ class storageManager extends defaultClass{
 		$this->db[$storage->getName()]=array('type'=>$storage->getType(),'storage'=>&$storage);
 		return true;
 	}
+	public function has($name){
+		return (isset($this->db[$name]))?true:false;
+	}
 	public function get($name){
 		return (isset($this->db[$name]))?($this->db[$name]['storage']->getHandler()):false;
 	}
@@ -17,6 +20,29 @@ class storageManager extends defaultClass{
 		$this->db[$name]['storage']->close();
 	}
 	public function loadDefaultStorage(){
+		try{
+			new storage('storage.default.config','fileConfig',CONFIGDIRECTORY.'storage.default'.EXT);
+			$entryc=intval($this->get('storage.default.config')->get('EntryCount'));
+			if($entryc>=1){
+				$defaults=array();
+				for($i=0;$i<$entryc;$i++){
+					$entry=array();
+					$entry['name']=$this->get('storage.default.config')->get('name','storage_'.$i);
+					$entry['path']=$this->get('storage.default.config')->get('path','storage_'.$i);
+					$entry['type']=$this->get('storage.default.config')->get('type','storage_'.$i);
+					$defaults[]=$entry;
+				}
+				foreach($defaults as $i=>$entry){
+					try{
+						new storage($entry['name'],$entry['type'],CONFIGDIRECTORY.$entry['path'].EXT);
+					}
+					catch(StorageException $e){
+						system::LOG()->w('StorageException',"Couldn't load default storage object: '".$entry['name']."' - ".$e->getMessage(),500);
+					}
+				}
+			}
+		}
+		catch(StorageException $e){}
 		try{
 			new storage('system.config','fileConfig',CONFIGDIRECTORY.'system'.EXT);
 		}
---
+++ store/config/storage.default.php
@@ -0,0 +1,9 @@
+; <?php die(); ?>
+
+[general]
+EntryCount = 1
+
+[storage_0]
+name = "db.config"
+path = "dbconfig"
+type = "fileConfig"
