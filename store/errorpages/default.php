0905c5ce482923c55b4a0cbd17062dc71119a099
Devimplode <jyhm.beem@googlemail.com>
2012-07-17 22:22:54 +0200

    misc: dark default errorpage look
    so use it maybe....

--- store/errorpages/default.php
+++ store/errorpages/default.php
@@ -1,12 +1,13 @@
 <html>
 	<head>
 		<title>Error - Oh noez, not again...</title>
+		<style>body{background:#151515}h1,h2,span,a{color:#DDD;font-family:tahoma,verdana,arial,sans-serif;line-height:2;}a{color:#6B84B4}a:hover{color:#3F8FBA}.h{font-weight:bold}hr{height:0;border:0;border-bottom:1px dashed #AAA}</style>
 	</head>
 	<body>
 		<h1>OMFG! An Error occurred!</h1>
 		<span>And I have no idea how I can beat them :(</span><br />
-		<span>But DON'T PANIC! we have some Peons to fix this!</span><br />
-		<span>They named the error "<?php echo($text) ?>". Please don't say this to the Peons, they are scared of them.</span><br /><br />
+		<span class="h">But DON'T PANIC!</span> <span>we have some Peons to fix this!</span><br />
+		<span>They named the error "<?php echo($text) ?>". Please don't say this to the Peons, they are scared of them.</span><br /><br /><hr />
 		<a href="https://www.google.de/search?q=cute+kitten&tbm=isch">So, how about some kittens?</a>
 	</body>
 </html>
