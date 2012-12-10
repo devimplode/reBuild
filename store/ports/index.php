649e3dc2e5ba6c86e5c65b2088309a13eac60423
Devimplode <jyhm.beem@googlemail.com>
2012-12-10 21:35:31 +0100

    ports/index.php - compatibility and db-demo

--- store/ports/index.php
+++ store/ports/index.php
@@ -1,9 +1,30 @@
+<!DOCTYPE>
 <html>
 	<head>
 		<title>It Works!</title>
 	</head>
 	<body>
 		<h1>It works!</h1>
-		<img src="//<?=system::RM()->request('host')?>/yay.png" alt="Yay!" />
+<?php
+	$url="mysqli://root@localhost:3306/test";
+	$db=system::SM()->get($url);
+	if($db && $db->test && $db->test->temp){
+		$data=$db->test->temp->select()->limit(3)->query()->getAssoc();
+		echo("<table>\n<tbody>\n<tr>\n");
+		foreach($data[0] as $k=>$v){
+			echo("<td>".$k."</td>\n");
+		}
+		echo("</tr>\n");
+		foreach($data as $id=>$arr){
+			echo("<tr>\n");
+			foreach($arr as $k=>$v){
+				echo("<td>".$v."</td>\n");
+			}
+			echo("</tr>\n");
+		}
+		echo("</tbody>\n</table>");
+	}
+?>
+		<img src="//<?php echo(system::RM()->request('host'))?>/yay.png" alt="Yay!" />
 	</body>
 </html>
